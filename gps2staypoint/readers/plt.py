87374a76a3a7dbbc93d2c779da5c237810fb8b53
Doug McGeehan <doug.mcgeehan@mst.edu>
2018-04-03 09:52:25 -0500

    speed increase due to not parsing lines immediately

--- gps2staypoint/readers/plt.py
+++ gps2staypoint/readers/plt.py
@@ -7,16 +7,10 @@ import dateutil.parser
 
 
 class PLTFileReader(object):
-    LATITUDE_INDEX = 0
-    LONGITUDE_INDEX = 1
-    DATE_INDEX = -2
-    TIME_INDEX = -1
-
     USER_ID_INDEX = -3
 
     def __init__(self, path):
         self.path = path
-        self.timestamped_locations = None
 
         # extract useful information from file
         self.user = int(path.split(os.sep)[self.USER_ID_INDEX])
@@ -26,9 +20,7 @@ class PLTFileReader(object):
         first_line = next(file)
         file.close()
 
-        split_line = self.split_line(line=first_line)
-
-        self.start_time = split_line['timestamp']
+        self.start_time = PLTPoint(line=first_line).timestamp
 
     def open(self):
         '''Open .plt file and skip the first few lines.'''
@@ -37,60 +29,53 @@ class PLTFileReader(object):
             next(gps_log)
         return gps_log
 
-    def load(self):
-        with open(self.path) as gps_log:
-            # Ignore the first six lines of each file
-            valid_lines = filter(lambda x: x[0] >= 6,
-                                 enumerate(gps_log))
-            # Split the lines into seperate fields
-            # '39.890275,116.453691,0,157,39925.4486111111,2009-04-22,10:46:00'
-            #       mapped to
-            # ['39.890275', '116.453691', '0', '157', '39925.4486111111',
-            #  '2009-04-22', '10:46:00']
-            raw_points = map(lambda line: line[1].rstrip().split(','),
-                             valid_lines)
-            # Extract only the lat, long, date, and time fields, merging the
-            #   date and time fields, and convert to the appropriate data types
-            # ['39.890275', '116.453691', '0', '157', '39925.4486111111',
-            #  '2009-04-22', '10:46:00']
-            #       mapped to
-            # [(39.890275, 116.453691), datetime.datetime(2009, 4, 22, 10, 46)]
-            points = map(
-                lambda raw_point: (
-                    (float(raw_point[self.LATITUDE_INDEX]),
-                     float(raw_point[self.LONGITUDE_INDEX])),
-                    dateutil.parser.parse(' '.join([
-                        raw_point[self.DATE_INDEX],
-                        raw_point[self.TIME_INDEX]
-                    ]))
-                ),
-                raw_points
-            )
-            self.timestamped_locations = list(points)
+    def __iter__(self):
+        log = self.open()
+        for line in log:
+            yield PLTPoint(line=line)
+        log.close()
 
-    def split_line(self, line):
-        split_line = line.rstrip().split(',')
-        return {
-            'latitude': float(split_line[self.LATITUDE_INDEX]),
-            'longitude': float(split_line[self.LONGITUDE_INDEX]),
-            'timestamp': dateutil.parser.parse(' '.join([
-                        split_line[self.DATE_INDEX],
-                        split_line[self.TIME_INDEX]
-            ]))
-        }
+    def __len__(self):
+        log = self.open()
+        return len(list(log))
 
-    def __getitem__(self, key):
-        # if isinstance(key, slice):
-        #     pass
-        #
-        # else:
-        #     pass
-        return self.timestamped_locations[key]
+
+class PLTPoint(object):
+    LATITUDE_INDEX = 0
+    LONGITUDE_INDEX = 1
+    DATE_INDEX = -2
+    TIME_INDEX = -1
+
+    def __init__(self, line):
+        # Split the lines into seperate fields
+        # '39.890275,116.453691,0,157,39925.4486111111,2009-04-22,10:46:00'
+        #       mapped to
+        # ['39.890275', '116.453691', '0', '157', '39925.4486111111',
+        #  '2009-04-22', '10:46:00']
+        self.line = line.rstrip().split(',')
 
     @property
-    def point_count(self):
-        return len(self.timestamped_locations)
+    def latitude(self):
+        # Extract only the lat, long, date, and time fields, merging the
+        #   date and time fields, and convert to the appropriate data types
+        # ['39.890275', '116.453691', '0', '157', '39925.4486111111',
+        #  '2009-04-22', '10:46:00']
+        #       mapped to
+        #     * latitude: 39.890275
+        #     * longitude: 116.453691
+        #     * timestamp: datetime.datetime(2009, 4, 22, 10, 46)
+        return float(self.line[self.LATITUDE_INDEX])
 
-    def long_lat_pairs(self):
-        return list(map(lambda p: (p[0][1], p[0][0]),
-                        self.timestamped_locations))
+    @property
+    def longitude(self):
+        return float(self.line[self.LONGITUDE_INDEX])
+
+    @property
+    def timestamp(self):
+        return dateutil.parser.parse(' '.join([
+            self.line[self.DATE_INDEX],
+            self.line[self.TIME_INDEX]
+        ]))
+
+    def __str__(self):
+        return '({0.latitude}, {0.longitude}) @{0.timestamp}'.format(self)
