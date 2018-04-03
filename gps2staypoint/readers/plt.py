4d98d357d2405781a4341e7de2b5cc77a0562038
Doug McGeehan <doug.mcgeehan@mst.edu>
2018-04-03 10:25:02 -0500

    moved gps trajectory to its own file-format-agnostic module

--- gps2staypoint/readers/plt.py
+++ gps2staypoint/readers/plt.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from gps2staypoint.gps import GPSPoint
+
 logger = logging.getLogger(__name__)
 
 import dateutil.parser
@@ -40,7 +42,7 @@ class PLTFileReader(object):
         return len(list(log))
 
 
-class PLTPoint(object):
+class PLTPoint(GPSPoint):
     LATITUDE_INDEX = 0
     LONGITUDE_INDEX = 1
     DATE_INDEX = -2
@@ -76,45 +78,3 @@ class PLTPoint(object):
             self.line[self.DATE_INDEX],
             self.line[self.TIME_INDEX]
         ]))
-
-    def __str__(self):
-        return '({0.latitude}, {0.longitude}) @{0.timestamp}'.format(self)
-
-
-class GPSTrajectory(object):
-    def __init__(self, time_interval_threshold, initial_point=None):
-        self.threshold = time_interval_threshold
-
-        self.points = []
-        if initial_point is not None:
-            self.points.append(initial_point)
-
-    def add_point(self, point):
-        if not self.points:
-            self.points.append(point)
-            return True
-
-        else:
-            time_difference = point.timestamp - self.latest_time
-            # logger.debug('Time difference between points: {}'.format(time_difference))
-            if time_difference >= self.threshold:
-                return False
-
-            else:
-                self.points.append(point)
-                return True
-
-    @property
-    def latest_time(self):
-        return self.points[-1].timestamp
-
-    def __str__(self):
-        start = self.points[0].timestamp
-        end = self.latest_time
-        return '{point_count: >4} points, {start} to {end} ' \
-               '({time_difference})'.format(
-            point_count=len(self.points),
-            start=start,
-            end=end,
-            time_difference=end-start,
-        )
